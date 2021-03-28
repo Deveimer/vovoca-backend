5927e7802d9355c079d2975936574e6735079954
kaustubh <kaustubh229@gmail.com>
2021-03-29 00:35:57 +0530

    --Deployment

--- database/pg.js
+++ database/pg.js
@@ -2,11 +2,12 @@ const pg = require('pg')
 const Pool = pg.Pool;
 
 const pool = new Pool({
-    database: "vovoca",
+    database: process.env.DATABASE || "vovoca",
     port: 5432,
-    host: "localhost",
-    user: "postgres",
-    password: process.env.MASTER_PASSWORD
+    host: process.env.HOST || "localhost",
+    user: process.env.USER || "postgres",
+    password: process.env.MASTER_PASSWORD || process.env.PASSWORD,
+    connectionString: process.env.DATABASE_URI
 })
 
 module.exports = pool
