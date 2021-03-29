c5d76d65213019aa32a8768cbe968d9b936d8cf6
kaustubh <kaustubh229@gmail.com>
2021-03-29 13:44:56 +0530

    Improvements

--- database/pg.js
+++ database/pg.js
@@ -5,7 +5,7 @@ const isProduction = process.env.NODE_ENV === 'production'
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 const pool = new Pool({
-    connectionString: isProduction ? connectionString : process.env.DATABASE_URI,
+    connectionString: isProduction ? connectionString : process.env.DATABASE_URL,
     ssl: isProduction
 })
 pool.connect(() => {
