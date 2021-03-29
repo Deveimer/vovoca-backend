69a2308a947bdc5590e0fb8af9cfc063a0547da8
kaustubh <kaustubh229@gmail.com>
2021-03-29 13:37:07 +0530

    Improvements

--- database/pg.js
+++ database/pg.js
@@ -5,8 +5,11 @@ const isProduction = process.env.NODE_ENV === 'production'
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 const pool = new Pool({
-    connectionString: isProduction ? connectionString | process.env.DATABASE_URI,
+    connectionString: isProduction ? connectionString : process.env.DATABASE_URI,
     ssl: isProduction
+})
+pool.connect(() => {
+    console.log("Database is up");
 })
 
 module.exports = pool
